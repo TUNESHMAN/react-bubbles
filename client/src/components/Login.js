ff2f25be5ff5c9aa6b5d4d24692893b64f774948
Babatunde <babatundea15@gmail.com>
2019-12-20 09:39:51 +0100

    I created the outlay of the login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
+import { React, useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: ""
+  });
+  // const onLogin = ({ username, password }) => {
+  //   return props.onLogin({ username, password });
+  // };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  handleChange = e => {
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          onChange={handleChange}
+          value={loginForm.username}
+        />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+          value={loginForm.password}
+        />
+        <br/>
+        <input type="submit"/>
+      </form>
+      </div>
     </>
   );
 };
