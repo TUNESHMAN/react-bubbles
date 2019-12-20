d1493e1d0329d33e27bd5157851acbdeecf85e61
Babatunde <babatundea15@gmail.com>
2019-12-20 10:22:31 +0100

    i made a post request whenever a login attempt is made on the api

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,24 +1,42 @@
-import { React, useState } from "react";
-import { Formik, Form, Field } from "formik";
+import  React, {useRef } from "react";
+import axios from 'axios';
 
-const Login = () => {
-  const [loginForm, setLoginForm] = useState({
-    username: "",
-    password: ""
-  });
+
+const Login = (props) => {
+
+  const usernameRef =useRef();
+  const passwordRef = useRef();
+  
   // const onLogin = ({ username, password }) => {
   //   return props.onLogin({ username, password });
   // };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  handleChange = e => {
-    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-  };
+  const submit = ()=>{
+    axios.post('http://localhost:5000/api/login',{
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    })
+    .then((res)=>{
+      // SUCCESS! Credentials are valid:
+      // 1 - The token string is then stored in local storage under a token key
+      localStorage.setItem('token', res.data.payload)
+      // Redirect users to the /colors route
+      props.history.push('/colors')
+
+    })
+
+    .catch((error)=>{
+      // This error messages comes up when the credentials are invalid.
+      alert(error.message)
+
+    })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <div>
-      <form>
+      {/* <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
@@ -37,7 +55,7 @@ const Login = () => {
         />
         <br/>
         <input type="submit"/>
-      </form>
+      </form> */}
       </div>
     </>
   );
